69d3214cb1ac351ac5d4c3bbbfbf0ef496b056d2
guishu <guillaume@schub.fr>
2021-12-17 21:13:39 +0100

    create window and main loop

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+def main():
+    pygame.init()
+
+    pygame.display.set_caption("Reversi")
+
+    screen = pygame.display.set_mode((800, 600))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+if __name__ == "__main__":
+    main()
