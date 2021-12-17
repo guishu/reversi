6ef88a8d36ccfaa288099d5c1a5261485cc7b4d2
guishu <guillaume@schub.fr>
2021-12-17 22:03:46 +0100

    use Board class to compute correct coordinates

--- main.py
+++ main.py
@@ -2,10 +2,48 @@ import pygame
 
 
 class Board:
-    def __init__(self):
+    def __init__(self, area_size):
         self.background_color = pygame.Color("green4")
-        self.pos = (0, 0)
-        self.size = (400, 400)
+        self.foreground_color = pygame.Color("black")
+
+        self.tile_size = Board._compute_tile_size(area_size)
+
+        self.size = 8 * self.tile_size
+        self.pos = (
+                (area_size[0] - self.size) // 2,
+                (area_size[1] - self.size) // 2,
+        )
+
+    @staticmethod
+    def _compute_tile_size(area_size):
+        smallest = min(area_size[0], area_size[1])
+
+        return smallest // 8
+
+    def render(self, surface):
+        surface.fill(self.background_color, self.pos + (self.size, self.size))
+
+        left = self.pos[0]
+        top = self.pos[1]
+        right = left + self.size
+        bottom = top + self.size
+
+        for i in range(1, 8):
+            offset = self.tile_size * i
+            pygame.draw.line(surface, self.foreground_color, (left + offset, top), (left + offset, bottom))
+            pygame.draw.line(surface, self.foreground_color, (left, top + offset), (right, top + offset))
+
+    def render_token(self, surface, player, x, y):
+        if player == 0:
+            color = pygame.Color("white")
+        else:
+            color = pygame.Color("black")
+
+        pygame.draw.circle(
+            surface,
+            color,
+            (self.pos[0] + self.tile_size * (x + 0.5), self.pos[1] + self.tile_size * (y + 0.5)),
+            self.tile_size * 0.4)
 
 
 def main():
@@ -14,6 +52,8 @@ def main():
     pygame.display.set_caption("Reversi")
 
     screen = pygame.display.set_mode((800, 600))
+
+    board = Board(screen.get_size())
 
     running = True
 
@@ -24,16 +64,12 @@ def main():
 
         screen.fill(pygame.Color("black"))
 
-        screen.fill(pygame.Color("green4"), (144, 44, 512, 512))
+        board.render(screen)
 
-        for i in range(1, 8):
-            pygame.draw.line(screen, pygame.Color("black"), (144 + 64 * i, 44), (144 + 64 * i, 556))
-            pygame.draw.line(screen, pygame.Color("black"), (144, 44 + 64 * i), (656 + 64 * i, 44 + 64 * i))
-
-        pygame.draw.circle(screen, pygame.Color("white"), (144 + 64 * 3 + 32, 44 + 64 * 3 + 32), 27)
-        pygame.draw.circle(screen, pygame.Color("white"), (144 + 64 * 4 + 32, 44 + 64 * 4 + 32), 27)
-        pygame.draw.circle(screen, pygame.Color("black"), (144 + 64 * 3 + 32, 44 + 64 * 4 + 32), 27)
-        pygame.draw.circle(screen, pygame.Color("black"), (144 + 64 * 4 + 32, 44 + 64 * 3 + 32), 27)
+        board.render_token(screen, 0, 3, 3)
+        board.render_token(screen, 0, 4, 4)
+        board.render_token(screen, 1, 3, 4)
+        board.render_token(screen, 1, 4, 3)
 
         pygame.display.flip()
 
